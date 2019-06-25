b734a3b62f205929dffe331ba41ad941e6809718
Jerome Brunel <jb.jeromebrunel@gmail.com>
2019-06-24 20:14:09 -0400

    Add Payment class

--- samples/server.js
+++ samples/server.js
@@ -3,10 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const tendopay = require('../');
 
-const app = express();
-
 const tendoPayClient = new tendopay.Client();
 tendoPayClient.enableSandbox();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.redirect('/cart');
@@ -15,8 +17,19 @@ app.get('/', (req, res) => {
 app.use('/cart', express.static('cart.html'));
 
 app.post('/purchase', (req, res) => {
-  // Handle payment
-  res.send('TODO Purchase');
+  const merchantId = 'TEST-OID-1234567890';
+  const orderAmount = +req.body.price;
+  const orderTitle = 'Test Order #1';
+
+  console.log(merchantId, orderAmount, orderTitle);
+
+  const tendoPayPayment = new tendopay.Payment({
+    merchantId,
+    requestAmount: orderAmount,
+    description: orderTitle
+  });
+
+  res.end('TODO');
 });
 
 app.listen(8000, () => {
