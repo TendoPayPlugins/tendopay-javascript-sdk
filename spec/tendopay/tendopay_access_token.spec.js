604660aff29ffd69c59c55f6b3684e928917366a
Jerome Brunel <jb.jeromebrunel@gmail.com>
2019-06-29 16:04:25 -0400

    Add spec for access token

---
+++ spec/tendopay/tendopay_access_token.spec.js
@@ -0,0 +1,28 @@
+const AccessToken = require('../../lib/tendopay/tendopay_access_token').AccessToken;
+
+describe('TendoPay AccessToken', () => {
+  it ('should construct an empty AccessToken object when empty parameters', () => {
+    const accessToken = new AccessToken({});
+
+    expect(accessToken.type).toBeNull();
+    expect(accessToken.token).toBeNull();
+    expect(accessToken.isExpired()).toBe(true);
+  });
+
+  it ('should construct an AccessToken object with parameters', (done) => {
+    const accessToken = new AccessToken({
+      type: 'ttype',
+      token: 'ttoken',
+      expiresIn: 0.5 // 0.5 second
+    });
+
+    expect(accessToken.type).toBe('ttype');
+    expect(accessToken.token).toBe('ttoken');
+    expect(accessToken.isExpired()).toBe(false);
+
+    setTimeout(() => {
+      expect(accessToken.isExpired()).toBe(true);
+      done();
+    }, 1000);
+  });
+});
